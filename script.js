476a5d8144e45f2c9bd211d74df56a322efd70b5
HaffissaAszahrra <raraocin@gmail.com>
2025-05-18 22:29:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+// Saat halaman dimuat, tampilkan alert setelah 2 detik jika pengguna tidak interaksi
+let isUserActive = false;
+
+document.addEventListener("mousemove", () => {
+  isUserActive = true;
+});
+
+setTimeout(() => {
+  if (!isUserActive) {
+    alert("Masih di sana? 😄");
+  }
+}, 2000);
