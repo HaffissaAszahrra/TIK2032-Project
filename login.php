592b3bede8306f5c5243e9cda17c110c8f2a3ed7
HaffissaAszahrra <raraocin@gmail.com>
2025-06-03 21:09:55 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>Login Form</title>
+</head>
+<body>
+  <h2>Login</h2>
+  <form action="login.php" method="POST">
+    <label>Nama Pengguna:</label><br>
+    <input type="text" name="username" required><br><br>
+
+    <label>Password:</label><br>
+    <input type="password" name="password" required><br><br>
+
+    <button type="submit">Login</button>
+  </form>
+</body>
+</html>
